5634fe3a8cbe21b5697464cb44da743dd469ba78
Troy Delacruz <troy8076@yahoo.com>
2023-04-15 01:13:07 -0400

    Added css styling & header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import Header from './components/Header'
+
+
+
 function App() {
+
     return (
-        <h1>Hello from the app component!</h1>
+        <>
+            <Header text="Hello World" />
+            <div className='container'>
+                <h1>My App</h1>
+            </div>
+        </>
     )
 }
 
